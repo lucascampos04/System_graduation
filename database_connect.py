048b78f12f1faa477c8cc11da3b24cb59ad57c24
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-16 16:59:54 -0300

    Scritpt de conexão ao banco de dados

---
+++ database_connect.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+def connect_database():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="senha123456",
+            database="system_graduation"
+        )
+        return connection
+    except mysql.connector.Error as err:
+        print("Erro ao conectar ao banco de dados:", err)
+        return None
+    
+
+def close_database(connection):
+    if connection:
+        connection.close()
