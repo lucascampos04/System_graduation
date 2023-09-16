759d12199f668fcec3375f4795dfd862171babfc
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-16 17:00:18 -0300

    Refatorando

--- controller/functions.py
+++ controller/functions.py
@@ -6,9 +6,4 @@ def number_random():
 
 
 def methhod_brand_graduation():
-    name_course = simpledialog.askstring("Nome do curso ", "Nome do curso ")
-    name_faculty = simpledialog.askstring("Nome da faculdade", "Nome da faculdade")
-    name_people = simpledialog.askstring("Nome do representante ", "Nome do representante")
-    phone_people = simpledialog.askinteger("Numero do representante ", "Numero do representante ")
-    form_payment = simpledialog.askstring("Forma de pagamento", "Forma de pagamento")
-    value_payment = messagebox.showinfo("Valor a ser pago", number_random)
+    pass
