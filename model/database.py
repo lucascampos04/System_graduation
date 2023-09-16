1f17f1a0c98ef6b44144016308451e9ae96f6bb3
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-16 14:42:22 -0300

    commit

--- model/database.py
+++ model/database.py
@@ -3,7 +3,7 @@ import mysql.connection
 def connect_database():
     try:
         bank = mysql.connection.connect(
-            host="localhost", user="root", password="senha12345", database="system"
+            host="localhost", user="root", password="senha123456", database="system"
         )
     except mysql.connection.errors as err:
         print("Erro ao conectar no banco de dados", err)
