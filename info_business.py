db20a93407672e61075a81674e78e7c822b72174
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-16 16:59:32 -0300

    Pagina de dados da empresa feita

--- info_business.py
+++ info_business.py
@@ -1,5 +1,8 @@
 from tkinter import Button, Frame, Tk, messagebox, Label, simpledialog, filedialog
 import mysql.connector
+import webbrowser
+
+from infoBusinessRepository import get_name, get_email, get_phone, get_ende
 
 '''colors'''
 whitesmoke = "#dcdee8"
@@ -8,47 +11,16 @@ blackLight = "#3d3f47"
 blackButLight = "#585959"
 blue = "#213ac4"
 
-def connect_database():
-    try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="senha123456",
-            database="system_graduation"
-        )
-        return connection
-    except mysql.connector.Error as err:
-        print("Erro ao conectar ao banco de dados:", err)
-        return None
 
-def close_database(connection):
-    if connection:
-        connection.close()
+def link_pag():
+    url = "https://www.4devs.com.br/gerador_de_cnpj"
+    webbrowser.open(url)
 
-# Função para obter o nome da empresa do banco de dados
-def get_name(id):
-    try:
-        connection = connect_database()
-        if not connection:
-            return "Erro ao conectar ao banco de dados"
-
-        cursor = connection.cursor()
-        cursor.execute('SELECT nome FROM dados_empresa WHERE id_empresa = %s', (id,))
-        business_name = cursor.fetchone()
-        cursor.close()
-        close_database(connection)
-
-        if business_name:
-            return business_name[0]
-        else:
-            return "Empresa não encontrada"
-
-    except mysql.connector.Error as err:
-        print("Erro ao executar a consulta:", err)
-        return "Erro ao executar a consulta"
-    
 def window_info_business(id):
     business_name = get_name(id)
+    business_email = get_email(id)
+    business_phone = get_phone(id)
+    business_ende = get_ende(id)
 
     window = Tk()
     window.title("Informações da empresa")
@@ -58,15 +30,26 @@ def window_info_business(id):
 
     '''FRAMES'''
     frame_top = Frame(window, width=250, height=150, bg=blue)
-    frame_top.grid(row=0, column=9)
+    frame_top.grid(row=0, column=0, sticky="w")
 
+    frame_midle = Frame(window, width=250, height=250, bg=whitesmoke)
+    frame_midle.grid(row=1, column=0, sticky="w")
 
     '''LABEL'''
-    business_name = get_name(id)
-    name_business = Label(frame_top, text=business_name)
-    name_business.place(x=1)
+    get_name_business = Label(frame_top, text=business_name, font=("Arial 25 bold"), bg=blue, fg="white")
+    get_name_business.place(x=10, y=50)
 
+    get_ende_business = Label(frame_midle, text=f'Email : {business_email}' , bg=whitesmoke, font=("Arial 12 bold"))
+    get_ende_business.place(x=5, y=30)
 
+    get_phone_business = Label(frame_midle, text=f'Telefone : {business_phone}' , bg=whitesmoke, font=("Arial 12 bold"))
+    get_phone_business.place(x=5, y=70)
+
+    get_ende_business = Label(frame_midle, text=f'Endereço : {business_ende}' , bg=whitesmoke, font=("Arial 12 bold"))
+    get_ende_business.place(x=5, y=110)
+
+    button_vist_pag = Button(frame_midle, text="Viste nossa pagina", font=("Arial 15 bold"), relief="flat", bg=whitesmoke, command=link_pag)
+    button_vist_pag.place(x=10, y=180)
 
     window.mainloop()
 
