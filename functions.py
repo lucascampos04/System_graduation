4694a2bcace1ddc9b2e7caf10d518c41efd58a5a
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-17 09:05:13 -0300

    refatorando

--- functions.py
+++ functions.py
@@ -1,11 +1,23 @@
 import random
+from tkinter import (
+    messagebox, 
+    simpledialog, 
+    Tk, 
+    Frame, 
+    Label, 
+    Button, 
+    Entry, 
+    ttk)
+from brand_graduation import label_limit_convidado
+
+
 
 def number_random():
     number = random.choice(1000, 90000)
 
-def method_brand_graduation():
-    pass
+
     
+
     
     
 
